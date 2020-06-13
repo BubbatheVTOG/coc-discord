c5f3b2a39e88bcd44910f2ee821c843fc4549306
Bubba Lichvar <bubbathevtog@gmail.com>
2020-06-13 05:23:40 -0400

    added my own stuff for neovim.

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,23 @@ import * as O from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { Logger } from './utils/logger';
 
-const clientId:string = '694558978776105000';
+const clientId:string = '721172077033553950';
 const logger:Logger = new Logger('discord');
 const ELAPSE_UPDATE_DURATION:number = 10000;
+const largeImageKey = 'neovim-512';
 
-const setActivity = (client: Client, startTimestamp: number) => {
-  const details = pipe(
+const getFileIcon = (fileName: string): string => {
+  const ext = fileName.split('.').pop() || '';
+  switch (ext) {
+    case 'ts':
+      return 'typescript-512';
+    default:
+      return null;
+  }
+};
+
+const setActivity = (client: Client, startTimestamp: number): void => {
+  const details: string = pipe(
     O.fromNullable(workspace.uri),
     O.filter((x) => x.startsWith('file:///')),
     O.map((x) => x.substr(8)),
@@ -20,7 +31,7 @@ const setActivity = (client: Client, startTimestamp: number) => {
     O.toUndefined,
   );
 
-  const state = pipe(
+  const state: string = pipe(
     O.fromNullable(workspace.root),
     O.map((x) => x.split('/')),
     O.filter((xs) => xs.length > 0),
@@ -29,7 +40,14 @@ const setActivity = (client: Client, startTimestamp: number) => {
     O.toUndefined,
   );
 
-  client.setActivity({ state, details, startTimestamp, instance: false });
+  client.setActivity({
+    state,
+    details,
+    startTimestamp,
+    largeImageKey,
+    smallImageKey: getFileIcon(details) || ' ',
+    instance: false,
+  });
 };
 
 const activate = () => {
